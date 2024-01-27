0c0b8fddbd935f45df9b5486ce56c8710723e05d
Ikhmal33 <supersaiya629@gmail.com>
2024-01-27 18:48:50 +0700

    push

---
+++ practice8/escape_characters.php
@@ -0,0 +1,25 @@
+<?php
+
+//! Escape Character
+/* To insert characters that are illegal in a string, use an escape character.
+*/
+
+$x = "We are the so-called \"Vikings\" from the north.";
+
+echo $x;
+
+
+//* Other Escape Characters :
+/* 
+    \' : single quote
+    \" : double quote
+    \$ : PHP variables
+    \n : new line
+    \r : carriage return
+    \t : tab
+    \f : form feed
+    \ooo : octal value
+    \xhh : hex value
+*/
+
+?>
---
+++ practice8/hello.php
@@ -0,0 +1,9 @@
+<?php
+
+$nama = "Ikhmal Bayu Pamungkas";
+$kelas = "MM4";
+echo "Hello $nama <br>";
+print "Class $kelas";
+
+?>
+
